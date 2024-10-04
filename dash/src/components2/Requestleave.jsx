834f087b4aadefa7bd711c76a1fd49f3eb4c6c33
aman <amanm.ug23.cs@nitp.ac.in>
2024-10-05 03:49:44 +0530

    updated

--- dash/src/components2/Requestleave.jsx
+++ dash/src/components2/Requestleave.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './RequestLeave.css';
+import '/RequestLeave.css';
 
 const Requestleave = () => {
   const [startDate, setStartDate] = useState('');
